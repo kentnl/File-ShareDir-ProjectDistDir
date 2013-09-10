f8fae57805083546658f0d9f2fa881619b1f679a
Kent Fredric <kentfredric@gmail.com>
2013-09-10 23:35:51 +1200

    move default dir to maint-travis-ci

--- sync_tree.pl
+++ sync_tree.pl
@@ -9,16 +9,21 @@ my $root = find_dev('./');
 chdir "$root";
 
 sub git_subtree {
-    system('git','subtree', @_ ) == 0 or die "Git subtree had nonzero exit";
+    system( 'git', 'subtree', @_ ) == 0 or die "Git subtree had nonzero exit";
 }
 
 my $travis = 'https://github.com/kentfredric/travis-scripts.git';
-my $prefix = 'maint';
+my $prefix = 'maint-travis-ci';
 
 if ( not -d -e $root->child($prefix) ) {
-  git_subtree('add','--prefix=' . $prefix , $travis, 'master' );
+    git_subtree( 'add', '--prefix=' . $prefix, $travis, 'master' );
 }
 else {
-  git_subtree('pull','-m', 'Synchronise git subtree maint', '--prefix=' . $prefix , $travis, 'master' );
+    git_subtree(
+        'pull', '-m',
+        'Synchronise git subtree maint-travis-ci',
+        '--prefix=' . $prefix,
+        $travis, 'master'
+    );
 }
 
