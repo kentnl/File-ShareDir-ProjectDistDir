7b535b8a82584a2930841ab9f17e57b79b981de0
Kent Fredric <kentfredric@gmail.com>
2013-09-16 19:18:55 +1200

    Add tree sterlizier stuff

---
+++ sterilize_env.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl 
+use strict;
+use warnings;
+use utf8;
+
+if ( not exists $ENV{TRAVIS} ) {
+    die "Is not travis!";
+}
+for my $i (@INC) {
+    next if $i !~ /site/;
+    next if $i eq '.';
+#    printf "%s\n", $i;
+    system('find',$i, '-type', 'f', '-delete');
+    system('find',$i, '-depth', '-type', 'd', '-delete' );
+}
+
