125fa6aa780de7ffd7a1ecbe2b88a99ad069df87
Kent Fredric <kentfredric@gmail.com>
2013-09-26 06:14:46 +1200

    Build results of d1d9a5c (on master)

--- maint-travis-ci/sterilize_env.pl
+++ maint-travis-ci/sterilize_env.pl
@@ -3,14 +3,29 @@ use strict;
 use warnings;
 use utf8;
 
+sub diag {
+    print STDERR @_;
+    print STDERR "\n";
+}
+
+if ( not exists $ENV{STERILIZE_ENV} ) {
+    diag('STERILIZE_ENV unset');
+    exit 0;
+}
+if ( $ENV{STERILIZE_ENV} < 1 ) {
+    diag('STERLIZIE_ENV < 1, Not Sterilizing');
+    exit 0;
+}
 if ( not exists $ENV{TRAVIS} ) {
-    die "Is not travis!";
+    diag('Is not running under travis!');
+    exit 1;
 }
 for my $i (@INC) {
     next if $i !~ /site/;
     next if $i eq '.';
-#    printf "%s\n", $i;
-    system('find',$i, '-type', 'f', '-delete');
-    system('find',$i, '-depth', '-type', 'd', '-delete' );
+    diag( 'Sterilizing files in ' . $i );
+    system( 'find', $i, '-type', 'f', '-delete' );
+    diag( 'Sterilizing dirs in ' . $i );
+    system( 'find', $i, '-depth', '-type', 'd', '-delete' );
 }
 
