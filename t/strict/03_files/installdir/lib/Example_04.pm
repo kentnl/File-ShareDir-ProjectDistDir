ef7341a1c1dcd921caa9ecbf62626489a45ade74
Kent Fredric <kentfredric@gmail.com>
2014-02-28 23:40:08 +1300

    Make strict variation dir

---
+++ t/strict/03_files/installdir/lib/Example_04.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+package Example_04;
+
+use File::ShareDir::ProjectDistDir;
+
+use Path::Class::File;
+
+sub test {
+  return scalar Path::Class::File->new( dist_file( 'Example_04', 'file' ) )->slurp();
+}
+
+1;
